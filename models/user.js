b6ae2afc8b4b84fdc81385ecd2cf3cc267004a21
Samdeesh <samdeeshsingh001@gmail.com>
2022-11-15 19:37:51 +0530

    Define user schema methods

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new Schema(
   {
@@ -45,6 +47,32 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const SALT = process.env.SALT;
+
+  const salt = await bcryptjs.genSalt(parseInt(SALT));
+  this.password = await bcryptjs.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
+
+userSchema.methods.generateAuthToken = function () {
+  const SECRET = process.env.SECRET;
+  const payload = {
+    id: this._id,
+    email: this.email,
+    firstName: this.firstName,
+    lastName: this.lastName,
+  };
+  const token = jwt.sign(payload, SECRET);
+
+  return token;
+};
 
 const User = mongoose.model('User', userSchema);
 
