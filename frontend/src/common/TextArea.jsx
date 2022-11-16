bfdd9a38b34ee394fa2cf905f6ff9d18c3efe4fa
Samdeesh <samdeeshsingh001@gmail.com>
2022-11-16 19:22:40 +0530

    Build common text area component

---
+++ frontend/src/common/TextArea.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+const TextArea = (props) => {
+  const { id, label, cols, rows, error, ...rest } = props;
+  return (
+    <>
+      <Form.Group className='mb-3' controlId={id}>
+        {label && <Form.Label>Example textarea</Form.Label>}
+        <Form.Control
+          name={id}
+          as='textarea'
+          rows={rows}
+          cols={cols}
+          isInvalid={!!error}
+          {...rest}
+        />
+        {error && <Form.Text className='text-danger'>{error}</Form.Text>}
+      </Form.Group>
+    </>
+  );
+};
+
+TextArea.defaultProps = {
+  error: null,
+  placeholder: '',
+};
+
+export default TextArea;
