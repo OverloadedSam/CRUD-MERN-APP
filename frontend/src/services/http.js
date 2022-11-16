f0b22591a82b62d2cb027ed971aa0cb14cb2c4b2
Samdeesh <samdeeshsingh001@gmail.com>
2022-11-16 21:30:47 +0530

    Add http service

---
+++ frontend/src/services/http.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import auth from './authService';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.headers.common = {
+  Authorization: `Bearer ${auth.getAuthToken()}`,
+};
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+};
+
+export default http;
