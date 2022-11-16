d56ecbacbd9fc6ccb5a46151cdde7f3082bf4279
Samdeesh <samdeeshsingh001@gmail.com>
2022-11-16 21:05:04 +0530

    Fix footer not sticking to bottom on empty pages

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-dark text-light p-3 text-center'>
+    <footer className='footer bg-dark text-light p-3 text-center'>
       {' '}
       &#169; CRUD App, 2022
     </footer>
