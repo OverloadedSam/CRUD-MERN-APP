d7f4a4bb4d7020539449b4170bee8ed8c658fd5a
Samdeesh <samdeeshsingh001@gmail.com>
2022-11-17 15:09:47 +0530

    Implement redux state management to login user

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -35,9 +35,44 @@ const resetRegisterUser = () => (dispatch) => {
   dispatch({ type: actions.USER_REGISTER_RESET });
 };
 
+const loginUser = (payload) => async (dispatch) => {
+  dispatch({ type: actions.USER_LOGIN_REQUESTED });
+
+  try {
+    const { data } = await http.post('/login', payload);
+    dispatch({
+      type: actions.USER_LOGIN_SUCCEEDED,
+      payload: data.data,
+    });
+
+    const { _id, firstName, lastName, email, token } = data;
+    const user = {
+      id: _id,
+      name: `${firstName} ${lastName}`,
+      email: email,
+    };
+
+    auth.saveUserAndAuthToken(user, token);
+  } catch (error) {
+    dispatch({
+      type: actions.USER_LOGIN_FAILED,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+const resetLoginUser = () => async (dispatch) => {
+  dispatch({ type: actions.USER_LOGIN_RESET });
+};
+
 const userActions = {
   registerUser,
   resetRegisterUser,
+  loginUser,
+  resetLoginUser,
 };
 
 export default userActions;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/userReducer';
+import auth from '../services/authService';
+
+const rootReducer = {
+  userRegister: userReducer.userRegisterReducer,
+  userLogin: userReducer.userLoginReducer,
+};
+
+const user = auth.getCurrentUser();
+const token = auth.getAuthToken();
+
+const preloadedState = {
+  userLogin: {
+    isLoggedIn: !!(user && token),
+    user,
+    token,
+  },
+};
 
 const store = configureStore({
-  reducer: {
-    userRegister: userReducer.userRegisterReducer,
-  },
+  reducer: rootReducer,
+  preloadedState,
 });
 
 export default store;
